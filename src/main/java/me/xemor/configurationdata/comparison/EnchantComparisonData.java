29127e7b561bca6345faba88613311d9efe08587
Xemorr <31805746+Xemorr@users.noreply.github.com>
2021-08-12 16:15:20 +0100

    Fixed EnchantComparisonData, added NONE field. Bumped version to 1.14.5

--- src/main/java/me/xemor/configurationdata/comparison/EnchantComparisonData.java
+++ src/main/java/me/xemor/configurationdata/comparison/EnchantComparisonData.java
@@ -11,11 +11,13 @@ import java.util.Map;
 
 public class EnchantComparisonData {
 
-    Map<Enchantment, RangeData> enchantMap = new HashMap<>();
+    private boolean noEnchants;
+    private final Map<Enchantment, RangeData> enchantMap = new HashMap<>();
 
     public EnchantComparisonData(ConfigurationSection enchantSection) {
         for (Map.Entry<String, Object> entry : enchantSection.getValues(false).entrySet()) {
             String name = entry.getKey();
+            if ("NONE".equals(entry.getKey())) noEnchants = false;
             Enchantment enchant = Enchantment.getByKey(NamespacedKey.minecraft(name.toLowerCase()));
             if (entry.getValue() instanceof String) {
                 enchantMap.put(enchant, new RangeData((String) entry.getValue()));
@@ -24,9 +26,10 @@ public class EnchantComparisonData {
     }
 
     public boolean matches(Map<Enchantment, Integer> enchantments) {
+        if (enchantments.size() == 0 && enchantMap.size() > 0) return false;
         for (Map.Entry<Enchantment, Integer> entry : enchantments.entrySet()) {
             RangeData rangeData = enchantMap.get(entry.getKey());
-            if (rangeData == null) continue;
+            if (rangeData == null) return false;
             if (!rangeData.isInRange(entry.getValue())) return false;
         }
         return true;
