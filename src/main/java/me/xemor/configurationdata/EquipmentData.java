1fcc2d3616e435a7ee0baa7551c5a804b45119ac
Sam (Xemor) <samuelhollis@outlook.com>
2025-03-16 21:12:00 +0000

    Add null checks to EquipmentData

--- src/main/java/me/xemor/configurationdata/EquipmentData.java
+++ src/main/java/me/xemor/configurationdata/EquipmentData.java
@@ -7,22 +7,22 @@ import org.bukkit.inventory.EntityEquipment;
 
 public class EquipmentData {
 
-    @JsonProperty("head")
+    @JsonPropertyWithDefault
     @JsonAlias({"helmet"})
     private final EquipableData head = null;
-    @JsonProperty("chest")
+    @JsonPropertyWithDefault
     @JsonAlias({"chestplate"})
     private final EquipableData chest = null;
-    @JsonProperty("legs")
+    @JsonPropertyWithDefault
     @JsonAlias({"leggings"})
     private final EquipableData legs = null;
-    @JsonProperty("boots")
+    @JsonPropertyWithDefault
     @JsonAlias({"feet"})
     private final EquipableData feet = null;
-    @JsonProperty("mainhand")
+    @JsonPropertyWithDefault
     @JsonAlias("hand")
     private final EquipableData mainhand = null;
-    @JsonProperty("offhand")
+    @JsonPropertyWithDefault
     private final EquipableData offhand = null;
 
     public EquipableData getHead() {
@@ -52,17 +52,29 @@ public class EquipmentData {
     public final void applyEquipment(LivingEntity livingEntity) {
         EntityEquipment equipment = livingEntity.getEquipment();
         if (equipment == null) return;
-        equipment.setHelmet(head.item(), true);
-        equipment.setHelmetDropChance(head.droprate());
-        equipment.setChestplate(chest.item(), true);
-        equipment.setChestplateDropChance(chest.droprate());
-        equipment.setLeggings(legs.item(), true);
-        equipment.setLeggingsDropChance(legs.droprate());
-        equipment.setBoots(feet.item(), true);
-        equipment.setBootsDropChance(feet.droprate());
-        equipment.setItemInMainHand(mainhand.item(), true);
-        equipment.setItemInMainHandDropChance(mainhand.droprate());
-        equipment.setItemInOffHand(offhand.item(), true);
-        equipment.setItemInOffHandDropChance(offhand.droprate());
+        if (head != null) {
+            equipment.setHelmet(head.item(), true);
+            equipment.setHelmetDropChance(head.droprate());
+        }
+        if (chest != null) {
+            equipment.setChestplate(chest.item(), true);
+            equipment.setChestplateDropChance(chest.droprate());
+        }
+        if (legs != null) {
+            equipment.setLeggings(legs.item(), true);
+            equipment.setLeggingsDropChance(legs.droprate());
+        }
+        if (feet != null) {
+            equipment.setBoots(feet.item(), true);
+            equipment.setBootsDropChance(feet.droprate());
+        }
+        if (mainhand != null) {
+            equipment.setItemInMainHand(mainhand.item(), true);
+            equipment.setItemInMainHandDropChance(mainhand.droprate());
+        }
+        if (offhand != null) {
+            equipment.setItemInOffHand(offhand.item(), true);
+            equipment.setItemInOffHandDropChance(offhand.droprate());
+        }
     }
 }
