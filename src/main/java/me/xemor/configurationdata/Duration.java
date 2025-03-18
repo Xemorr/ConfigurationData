428f0d83baf032a50e9d0955710f96706c851c94
Sam (Xemor) <samuelhollis@outlook.com>
2025-03-18 13:47:09 +0000

    Add Duration class

---
+++ src/main/java/me/xemor/configurationdata/Duration.java
@@ -0,0 +1,23 @@
+package me.xemor.configurationdata;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.Optional;
+
+public class Duration {
+
+    private Double durationInSeconds = null;
+
+    @JsonCreator
+    public Duration(Double duration) {
+        this.durationInSeconds = duration;
+    }
+
+    public Optional<Double> getDurationInSeconds() {
+        return Optional.ofNullable(durationInSeconds);
+    }
+
+    public Optional<Long> getDurationInTicks() {
+        return Optional.ofNullable(durationInSeconds).map((it) -> Math.round(it * 20));
+    }
+}
